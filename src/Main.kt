6fa31a65cde8a91142c385111577a4c0aff008a8
waimea-bstodd <bstodd@waimea.school.nz>
2026-03-23 10:58:57 +1300

    Setting up board
    Added way to create and show squares

--- src/Main.kt
+++ src/Main.kt
@@ -11,9 +11,38 @@
  * =====================================================================
  */
 
-
+val squares = mutableListOf<String?>()
 
 fun main() {
-    println("Hello World!")
+    createCells()
+    showCells()
+
+    }
+
+
+fun createCells() {
+    for (i in 1..16){
+        squares.add("EMPTY")
+    } //make it so it will pick 4 spots for white and 1 for black and put them in random area
 }
+
+fun showCells() {
+    for (i in 1..squares.size) {
+        print("Square $i  ".padEnd(length = 11))
+    }
+    println()
+    print("┌──────────")
+    print("┬──────────".repeat(squares.size-1))
+    println("┐")
+
+    for (cell in squares){
+        print("│ ${cell?.padEnd(8)} ")
+    }
+    print("│")
+    println()
+    print("└──────────")
+    print("┴──────────".repeat(squares.size-1))
+    println("┘")
+}
+
 
